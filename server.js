821ee04601412331ee9e43fdfd42a6dac4988dd4
lukmana <laode.lukmana88@gmail.com>
2025-12-12 14:53:50 +0800

    inisialisasi projek

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// register
+app.get("/api/auth/register", (req, res) => {
+  res.send("Register Page");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
